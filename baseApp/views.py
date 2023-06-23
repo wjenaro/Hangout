0281325b11ed916f1a54d7207ed8561c52a523e5
Jenaro Wachira <wjenaro@gmail.com>
2023-06-23 19:38:17 +0300

    models

--- baseApp/views.py
+++ baseApp/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
+from .models import Room #kazi safi
 
-rooms= [
-    {'id':1, 'name': "let learn python"},
-    {'id':2, 'name': "let learn Javascript"},
-    {'id':3, 'name': "let learn PHP"},
-    {'id':4, 'name': "let learn HTML"},
-]
+#rooms= [
+  #  {'id':1, 'name': "let learn python"},
+    #{'id':2, 'name': "let learn Javascript"},
+    #{#'id':3, 'name': "let learn PHP"},
+    #{'id':4, 'name': "let learn HTML"},
+#]
 # Create your views here.
 def home(request):
+    '''Create model manager
+    '''
+    rooms=Room.objects.all()
+    ''''''
     context ={'rooms':rooms}
     return render(request, "baseApp/home.html", context)
 '''
 room view
 '''
 def room(request, pk):
-    room=None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room=i
+    room=Room.objects.get(id=pk)
+  
     '''
     set context dictionary
     '''
