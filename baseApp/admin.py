a0180d300f574d9d42ba5661cad26b937b725654
Jenaro Wachira <wjenaro@gmail.com>
2023-06-23 19:56:39 +0300

    updated db

--- baseApp/admin.py
+++ baseApp/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 #register models
 #start by importing the model
-from .models import Room
+from .models import Room, Topic, Message
 '''
 kasi safi
 '''
 admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
+
--- baseApp/models.py
+++ baseApp/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Topic(models.Model):
+    name=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 class Room(models.Model):
-    #host
-    #topic
+    host=models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic=models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name=models.CharField(max_length=200)
     description=models.TextField(null=True, blank=True)
     #participants=
@@ -26,8 +31,3 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
-class Topic(models.Model):
-    name=models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
